284597af69c04b1fe91cae204154b77ebab7dfe7
Henry Onyebuchi <50964581+Hendobox@users.noreply.github.com>
2020-02-06 13:17:27 +0100

    Update LeadToken.sol

--- LeadToken.sol
+++ LeadToken.sol
@@ -3,27 +3,17 @@ pragma solidity >=0.5.0 < 0.6.0;
 import "./TokenLock.sol";
 
 interface ERC20Interface {
-
     function totalSupply() external view returns (uint);
-
     function balanceOf(address _owner) external view returns (uint balance);
-
     function allowance(address _owner, address _spender) external view returns (uint remaining);
-
     function transfer(address _to, uint _tokens) external returns (bool success);
-
     function approve(address _spender, uint _tokens) external returns (bool success);
-
     function transferFrom(address _from, address _to, uint _tokens) external returns (bool success);
-
-
     event Transfer(address indexed _from, address indexed _to, uint _tokens);
-
     event Approval(address indexed _owner, address indexed _spender, uint _tokens);
-
 }
 
-contract LeadToken is TokenLock {
+contract LeadToken is ERC20Interface {
 
     // Public variables of the LeadToken
     string public name = "LeadToken";
@@ -65,6 +55,13 @@ contract LeadToken is TokenLock {
         totalSupply = _initialSupply * 10 ** uint(decimals); 
         balances[msg.sender] = totalSupply;
         emit Transfer(address(0x0), msg.sender, totalSupply);
+    }
+    
+    /**
+     * Returns the total suppy of the LEAD token
+     */
+    function totalSupply() external view returns (uint) {
+        return _totalSupply;
     }
     
     /**
